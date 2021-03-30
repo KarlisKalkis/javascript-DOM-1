4b16627bda91af59d7deb54266a9d8ce53f9b1f9
KarlisKalkis <kkalkis21@gmail.com>
2021-03-30 17:01:49 +0300

    5.uzdevums bez atbildem

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,21 @@
 // Your JavaScript goes here.
+
+window.addEventListener('load',function(){
+
+    const elements = document.querySelectorAll(".second_five li");
+    const button = document.querySelector('#toggle_button');
+
+    var toggleStatus  = false
+    button.addEventListener('click', function(e){
+        elements.forEach(function(element){
+            if (toggleStatus == false) {
+                element.style.display = 'flex';
+            }else {
+                element.style.display = 'none';
+            }
+        })
+        toggleStatus = !toggleStatus
+    })
+
+
+})
